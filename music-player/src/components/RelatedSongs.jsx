7fe7429dc16d9f797b4f727bda562e3de9948331
Rustam Rustamov <rustam.r.r.r.r@gmail.com>
2022-10-13 22:35:32 +0300

    songPage

--- music-player/src/components/RelatedSongs.jsx
+++ music-player/src/components/RelatedSongs.jsx
@@ -1,5 +1,22 @@
-const RelatedSongs = () => (
-  <div>Loader</div>
-);
+import SongBar from './SongBar';
 
+const RelatedSongs = ({ data, isPlaying, activeSong, handlePause, handlePlay, artistId }) => (
+  <div className='flex flex-col '>
+    <h1 className='font-bold text-3xl text-white'>Related Songs:</h1>
+    <div clasName='mt-6 w-full flex flex-col '>
+      {data?.map((song, i) => (
+        <SongBar
+          song={song}
+          key={`${song.key}-${artistId}`}
+          i={i}
+          artistId={artistId}
+          isPlaying={isPlaying}
+          activeSong={activeSong}
+          handlePlay={handlePlay}
+          handlePause={handlePause}
+        />
+      ))}
+    </div>
+  </div>
+);
 export default RelatedSongs;
--- music-player/src/components/TopPlay.jsx
+++ music-player/src/components/TopPlay.jsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { FreeMode } from 'swiper';
+// eslint-disable-next-line import/no-unresolved
 import 'swiper/css';
+// eslint-disable-next-line import/no-unresolved
 import 'swiper/css/free-mode';
 import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
@@ -13,14 +15,14 @@ const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick, handle
   <div
     className="w-full flex flex-row hover:bg-[#4c426e] py02 p-4 rounded-jg cursor-pointer mb-2"
   ><h3 className="font-bold text-base text-white mr-3">{i + 1}</h3>
-    <div className='flex-1 flex flex-row justify-between items-center'>
-      <img src={song?.images?.coverart} alt={song?.title} className='w-20 h-20 rounded-lg' />
-      <div className='flex-1 flex flex-col justify-center mx-3'>
+    <div className="flex-1 flex flex-row justify-between items-center">
+      <img src={song?.images?.coverart} alt={song?.title} className="w-20 h-20 rounded-lg" />
+      <div className="flex-1 flex flex-col justify-center mx-3">
         <Link to={`/songs/${song.key}`}>
-          <p className='text-xl font-bold text-white'>{song.title}</p>
+          <p className="text-xl font-bold text-white">{song.title}</p>
         </Link>
         <Link to={`/songs/${song?.artists[0].adamid}`}>
-          <p className='text-base font-bold text-gray-300 mt-1'>{song.subtitle}</p>
+          <p className="text-base font-bold text-gray-300 mt-1">{song.subtitle}</p>
         </Link>
 
       </div>
