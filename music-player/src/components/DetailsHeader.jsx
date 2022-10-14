31843bc2e01fab046f0c3a95dbfe738d08701b01
Rustam Rustamov <rustam.r.r.r.r@gmail.com>
2022-10-14 21:44:57 +0300

    contry-almost-finished

--- music-player/src/components/DetailsHeader.jsx
+++ music-player/src/components/DetailsHeader.jsx
@@ -3,22 +3,24 @@ import { Link } from 'react-router-dom';
 const DetailsHeader = ({ artistId, artistData, songData }) => {
   const artist = artistData?.artists[artistId].attributes;
   return (
-    <div className='relative w-full flex flex-col mt-20'>
-      <div className='w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28'></div>
-      <div className='absolute inset-0 flex item-center'><img src={artistData ? artist?.artwork?.url.replace('{w}', '500').replace('{h}', '500') : songData.images?.coverart} alt="art" className='sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black' />
-        <div className='flex flex-col ml-10 justify-center'>
-          <div className='ml-5'>
-            <p className='font-bold sm:text-3xl text-xl text-white'>{artistId ? artist?.name : songData?.title}</p>
+    <div className="relative w-full flex flex-col mt-20">
+      <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
+      <div className="absolute inset-0 flex item-center"><img src={artistData ? artist?.artwork?.url.replace('{w}', '500').replace('{h}', '500') : songData.images?.coverart} alt="art" className="sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black" />
+        <div className="flex flex-col ml-10 justify-center">
+          <div className="ml-5">
+            <p className="font-bold sm:text-3xl text-xl text-white">{artistId ? artist?.name : songData?.title}</p>
           </div>
-          {!artistId &&
+          {!artistId
+            && (
             <Link to={`/artists/${songData.artists[0].adamid}`}>
               <p className="text-base text-gray-400 mt-2">{songData?.subtitle}</p>
-            </Link>}
+              </Link>
+            )}
           <p className="text-base text-gray-400 mt-2">{artistId ? artist?.genreNames[0] : songData?.genres?.primary}</p>
         </div>
       </div>
-      <div className="w-full sm:h-44 h-24 "></div>
+      <div className="w-full sm:h-34 h-24 " />
     </div>
   );
-}
+};
 export default DetailsHeader;
--- music-player/src/pages/ArtistDetails.jsx
+++ music-player/src/pages/ArtistDetails.jsx
@@ -1,47 +1,33 @@
 import { useParams } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
-import { setActiveSong, playPause } from '../redux/features/playerSlice';
-import { useGetSongDetailsQuery, useGetSongRelatedQuery } from '../redux/services/shazamCore';
-
-
+import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
 
 const ArtistDetails = () => {
-  const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
 
 
-  const { songid } = useParams();
-  const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
-  const { data, isFetching: isFetchingRelatedSongs, error } = useGetSongRelatedQuery({ songid });
+  const { id: artistId } = useParams();
+  const { data: artistData, isFetching: isFetchingArtistData, error } = useGetArtistDetailsQuery(artistId);
 
-  const handlePauseClick = () => {
-    dispatch(playPause(false))
-  };
-  const handlePlayClick = (song, i) => {
-    dispatch(setActiveSong({ song, data, i }))
-    dispatch(playPause(true))
-  };
-  if (isFetchingRelatedSongs || isFetchingSongDetails) {
-    return <Loader title="Fetching song details" />;
+
+
+  if (isFetchingArtistData) {
+    return <Loader title="Fetching artist details" />;
   }
+  if (error) return <Error title="Error fetching artist details" />;
 
   return (
     <div className="flex flex-col">
-      <DetailsHeader artistId="" songData={songData} />
-      <div className="mb-10">
-        <h2 className="text-white text-3xl font-bold">Lyrics: </h2>
-        <div className="mt-5">
-          {songData?.sections[1].type === 'LYRICS' ? songData?.sections[1].text.map((line, i) => (<p className="text-gray-400 text-base my-1">{line}</p>)) : <p className="text-gray-400 text-base my-1">Sorry no lyrics found.</p>}
-        </div>
-      </div>
+      <DetailsHeader
+        artistId={artistId}
+        artistData={artistData}
+      />
       <RelatedSongs
-        data={data}
+        data={Object?.values(artistData?.songs)}
+        artistId={artistId}
         isPlaying={isPlaying}
         activeSong={activeSong}
-        handlePause={handlePauseClick}
-        handlePlay={handlePlayClick}
-
       />
     </div>
   );
